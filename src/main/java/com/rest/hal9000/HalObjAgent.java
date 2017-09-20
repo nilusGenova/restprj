8fc8dba7796aec587b6e93fa774d50a0da070875
nilusGenova <albertofirpo12@gmail.com>
2017-09-20 22:18:53 +0200

    starting agent implementations

---
+++ src/main/java/com/rest/hal9000/HalObjAgent.java
@@ -0,0 +1,17 @@
+package com.rest.hal9000;
+
+public class HalObjAgent {
+	
+	private final char id;
+	
+	public HalObjAgent(char id) {
+		super();
+		this.id = id;
+	}
+
+	public char getId() {
+		return id;
+	}
+	
+	
+}
