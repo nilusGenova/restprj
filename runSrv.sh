2c3d6369342248cb4b98e78762d5dea048fb8618
nilusGenova <albertofirpo12@gmail.com>
2018-01-25 15:12:36 +0100

    Fix log files rigths

--- runSrv.sh
+++ runSrv.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-#java -DLogPath=/tmp/ -DDebugLogLevel=defined -jar target/restprj-1.1-SNAPSHOT.jar
-java -DLogPath=/tmp/ -jar target/restprj-1.1-SNAPSHOT.jar
+
+pathlog="/tmp/"
+for file in $pathlog*.log
+do 
+     echo "File: ${file}"
+     touch ${file}
+     chmod g+w ${file}
+done
+
+#java -DLogPath=$pathlog -DDebugLogLevel=defined -jar target/restprj-1.1-SNAPSHOT.jar
+java -DLogPath=$pathlog -jar target/restprj-1.1-SNAPSHOT.jar
 
